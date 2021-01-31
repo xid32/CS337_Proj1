a82c55774bba421bde0bb124a4256a71a2397ea8
Gan Qiu <ganqiu@GQQ-2.local>
2021-01-31 12:17:57 -0600

    a better way to match award in text

--- src/get_presenters.py
+++ src/get_presenters.py
@@ -13,12 +13,16 @@ OFFICIAL_AWARDS = ['cecil b. demille award',
                    'best motion picture - comedy or musical', 
                    'best performance by an actress in a motion picture - comedy or musical', 
                    'best performance by an actor in a motion picture - comedy or musical', 
-                   'best animated feature film', 'best foreign language film', 
+                   'best animated feature film', 
+                   'best foreign language film', 
                    'best performance by an actress in a supporting role in a motion picture', 
                    'best performance by an actor in a supporting role in a motion picture', 
-                   'best director - motion picture', 'best screenplay - motion picture', 
-                   'best original score - motion picture', 'best original song - motion picture', 
-                   'best television series - drama', 'best performance by an actress in a television series - drama', 
+                   'best director - motion picture', 
+                   'best screenplay - motion picture', 
+                   'best original score - motion picture', 
+                   'best original song - motion picture', 
+                   'best television series - drama', 
+                   'best performance by an actress in a television series - drama', 
                    'best performance by an actor in a television series - drama', 
                    'best television series - comedy or musical', 
                    'best performance by an actress in a television series - comedy or musical', 
@@ -29,6 +33,35 @@ OFFICIAL_AWARDS = ['cecil b. demille award',
                    'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 
                    'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
+
+AWARD_SCRIPTS = {'cecil b. demille award': ["cecil", "demille"],
+                 'best motion picture - drama': ['motion', 'drama'],
+                 'best performance by an actress in a motion picture - drama': ["actress", "motion", "drama"],
+                 'best performance by an actor in a motion picture - drama': ["actor", "motion", "drama"],
+                 'best motion picture - comedy or musical': ["motion", "comedy", "musical"],
+                 'best performance by an actress in a motion picture - comedy or musical': ["actress", "comedy", "musical"],
+                 'best performance by an actor in a motion picture - comedy or musical': ["actor", "comedy", "musical"],
+                 'best animated feature film': ["animated"],
+                 'best foreign language film': ["foreign language"],
+                 'best performance by an actress in a supporting role in a motion picture': ["actress", "supporting", "motion"],
+                 'best performance by an actor in a supporting role in a motion picture': ["actor", "supporting", "motion"],
+                 'best director - motion picture': ["director", "motion"],
+                 'best screenplay - motion picture': ["screenplay", "motion"],
+                 'best original score - motion picture': ["score", "motion"],
+                 'best original song - motion picture': ["song", "motion"],
+                 'best television series - drama': ["series", "drama"],
+                 'best performance by an actress in a television series - drama': ["actress", "series", "drama"],
+                 'best performance by an actor in a television series - drama':["actor", "series", "drama"], 
+                 'best television series - comedy or musical': ["television series", "comedy", "musical"],
+                 'best performance by an actress in a television series - comedy or musical': ["actress", "series", "comedy", "musical"],
+                 'best performance by an actor in a television series - comedy or musical': ["actor", "series", "comedy", "musical"],
+                 'best mini-series or motion picture made for television': ["mini series"],
+                 'best performance by an actress in a mini-series or motion picture made for television': ["actress", "mini series"],
+                 'best performance by an actor in a mini-series or motion picture made for television': ["actor", "mini series"],
+                 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television': ["actress", "supporting", "mini series"],
+                 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television': ["actor", "supporting", "mini series"]}
+
+
 def cleanString(s):
     return "".join(" " if i in punctuation else i for i in s.strip(punctuation))
 
@@ -37,6 +70,21 @@ def remove_punc(s):
 
 def get_dict_larger_than(d, threshold):
     return dict((k, v) for k, v in d.items() if v >= threshold)
+
+def isContainAll(text, keyWords):
+    for keyWord in keyWords:
+        if keyWord.lower() not in text:
+            return False
+    return True
+
+def get_answers():
+    answer_dict= {}
+    answer = {"hosts": ["amy poehler", "tina fey"], "award_data": {"best screenplay - motion picture": {"nominees": ["zero dark thirty", "lincoln", "silver linings playbook", "argo"], "presenters": ["robert pattinson", "amanda seyfried"], "winner": "django unchained"}, "best director - motion picture": {"nominees": ["kathryn bigelow", "ang lee", "steven spielberg", "quentin tarantino"], "presenters": ["halle berry"], "winner": "ben affleck"}, "best performance by an actress in a television series - comedy or musical": {"nominees": ["zooey deschanel", "tina fey", "julia louis-dreyfus", "amy poehler"], "presenters": ["aziz ansari", "jason bateman"], "winner": "lena dunham"}, "best foreign language film": {"nominees": ["the intouchables", "kon tiki", "a royal affair", "rust and bone"], "presenters": ["arnold schwarzenegger", "sylvester stallone"], "winner": "amour"}, "best performance by an actor in a supporting role in a motion picture": {"nominees": ["alan arkin", "leonardo dicaprio", "philip seymour hoffman", "tommy lee jones"], "presenters": ["bradley cooper", "kate hudson"], "winner": "christoph waltz"}, "best performance by an actress in a supporting role in a series, mini-series or motion picture made for television": {"nominees": ["hayden panettiere", "archie panjabi", "sarah paulson", "sofia vergara"], "presenters": ["dennis quaid", "kerry washington"], "winner": "maggie smith"}, "best motion picture - comedy or musical": {"nominees": ["the best exotic marigold hotel", "moonrise kingdom", "salmon fishing in the yemen", "silver linings playbook"], "presenters": ["dustin hoffman"], "winner": "les miserables"}, "best performance by an actress in a motion picture - comedy or musical": {"nominees": ["emily blunt", "judi dench", "maggie smith", "meryl streep"], "presenters": ["will ferrell", "kristen wiig"], "winner": "jennifer lawrence"}, "best mini-series or motion picture made for television": {"nominees": ["the girl", "hatfields & mccoys", "the hour", "political animals"], "presenters": ["don cheadle", "eva longoria"], "winner": "game change"}, "best original score - motion picture": {"nominees": ["argo", "anna karenina", "cloud atlas", "lincoln"], "presenters": ["jennifer lopez", "jason statham"], "winner": "life of pi"}, "best performance by an actress in a television series - drama": {"nominees": ["connie britton", "glenn close", "michelle dockery", "julianna margulies"], "presenters": ["nathan fillion", "lea michele"], "winner": "claire danes"}, "best performance by an actress in a motion picture - drama": {"nominees": ["marion cotillard", "sally field", "helen mirren", "naomi watts", "rachel weisz"], "presenters": ["george clooney"], "winner": "jessica chastain"}, "cecil b. demille award": {"nominees": [], "presenters": ["robert downey, jr."], "winner": "jodie foster"}, "best performance by an actor in a motion picture - comedy or musical": {"nominees": ["jack black", "bradley cooper", "ewan mcgregor", "bill murray"], "presenters": ["jennifer garner"], "winner": "hugh jackman"}, "best motion picture - drama": {"nominees": ["django unchained", "life of pi", "lincoln", "zero dark thirty"], "presenters": ["julia roberts"], "winner": "argo"}, "best performance by an actor in a supporting role in a series, mini-series or motion picture made for television": {"nominees": ["max greenfield", "danny huston", "mandy patinkin", "eric stonestreet"], "presenters": ["kristen bell", "john krasinski"], "winner": "ed harris"}, "best performance by an actress in a supporting role in a motion picture": {"nominees": ["amy adams", "sally field", "helen hunt", "nicole kidman"], "presenters": ["megan fox", "jonah hill"], "winner": "anne hathaway"}, "best television series - drama": {"nominees": ["boardwalk empire", "breaking bad", "downton abbey (masterpiece)", "the newsroom"], "presenters": ["salma hayek", "paul rudd"], "winner": "homeland"}, "best performance by an actor in a mini-series or motion picture made for television": {"nominees": ["benedict cumberbatch", "woody harrelson", "toby jones", "clive owen"], "presenters": ["jessica alba", "kiefer sutherland"], "winner": "kevin costner"}, "best performance by an actress in a mini-series or motion picture made for television": {"nominees": ["nicole kidman", "jessica lange", "sienna miller", "sigourney weaver"], "presenters": ["don cheadle", "eva longoria"], "winner": "julianne moore"}, "best animated feature film": {"nominees": ["frankenweenie", "hotel transylvania", "rise of the guardians", "wreck-it ralph"], "presenters": ["sacha baron cohen"], "winner": "brave"}, "best original song - motion picture": {"nominees": ["act of valor", "stand up guys", "the hunger games", "les miserables"], "presenters": ["jennifer lopez", "jason statham"], "winner": "skyfall"}, "best performance by an actor in a motion picture - drama": {"nominees": ["richard gere", "john hawkes", "joaquin phoenix", "denzel washington"], "presenters": ["george clooney"], "winner": "daniel day-lewis"}, "best television series - comedy or musical": {"nominees": ["the big bang theory", "episodes", "modern family", "smash"], "presenters": ["jimmy fallon", "jay leno"], "winner": "girls"}, "best performance by an actor in a television series - drama": {"nominees": ["steve buscemi", "bryan cranston", "jeff daniels", "jon hamm"], "presenters": ["salma hayek", "paul rudd"], "winner": "damian lewis"}, "best performance by an actor in a television series - comedy or musical": {"nominees": ["alec baldwin", "louis c.k.", "matt leblanc", "jim parsons"], "presenters": ["lucy liu", "debra messing"], "winner": "don cheadle"}}}
+    for key in answer["award_data"]:
+        answer_dict[key] = answer["award_data"][key]["presenters"]
+    return answer_dict
+
+
 
 # TODO: Mine the names instead of hard code 
 def get_names():
@@ -51,17 +99,39 @@ def get_names():
 
 names = get_names()
 
+
+
 # TODO: A better way to match award in text
 def vote_for_each_award(awards, names):    
-        for award in awards:
-            for i in range(0, len(data)):
-                text = remove_punc(data[i]["text"].lower())
-                if remove_punc(award).lower() in text:
-                    for key in names:
-                        if remove_punc(key).lower() in text:
-                         names[key] += 1
+    for award in awards:
+        for i in range(0, len(data)):
+            text = remove_punc(data[i]["text"].lower())
+            if remove_punc(award).lower() in text:
+                for key in names:
+                    if remove_punc(key).lower() in text:
+                        names[key] += 1
 
-            print(award, get_dict_larger_than(names, 1))
-            names = dict.fromkeys(names, 0)
+        print(award, get_dict_larger_than(names, 1))
+        names = dict.fromkeys(names, 0)
 
-vote_for_each_award(OFFICIAL_AWARDS, names)
+# vote_for_each_award(OFFICIAL_AWARDS, names)
+
+def v2(awards, scripts, names, answer_dict):
+    k = 0
+    for award in OFFICIAL_AWARDS:
+        for i in range(0, len(data)):
+            text = remove_punc(data[i]["text"].lower())
+            keyWords = scripts[award]
+            if isContainAll(text, keyWords):
+                for key in names:
+                    if remove_punc(key).lower() in text:
+                        names[key] += 1
+        new_dict = get_dict_larger_than(names, 1)
+        print("Award: ", award)
+        print("     my   answers: ,", sorted(new_dict, key=new_dict.get, reverse=True)[:2])
+        print("     true answers: ", *answer_dict[award], sep = ", ")
+        print("\n")
+        names = dict.fromkeys(names, 0)
+        k += 1
+
+v2(OFFICIAL_AWARDS, AWARD_SCRIPTS, names, get_answers())
