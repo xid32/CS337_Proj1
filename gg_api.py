26d996ece07afffc69ff08baeea391263c9108de
chend666 <44977686+chend666@users.noreply.github.com>
2021-02-12 19:47:26 -0600

    Update gg_api.py

--- gg_api.py
+++ gg_api.py
@@ -9,6 +9,7 @@ from get_presenters import get_presenters_results as gr
 from get_dress import get_dress as gd
 from get_sentiments import get_sentiments_results
 import time
+import sys
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
@@ -35,10 +36,7 @@ def get_nominees(year):
     names as keys, and each entry a list of strings. Do NOT change
     the name of this function or what it returns.'''
     # Your code here
-    gs.get_nominees(year)
-    f = open(str(year) + '_nominees' + '.json')
-    nominees = json.load(f)
-    return nominees
+    return get_answers(year)['nominees']
 
 def get_winner(year):
     '''Winners is a dictionary with the hard coded award
@@ -54,13 +52,12 @@ def get_presenters(year):
     # Your code here
     return get_answers(year)['presenters']
 
-def pre_ceremony():
+def pre_ceremony(years):
     '''This function loads/fetches/processes any data your program
     will use, and stores that data in your DB or in a json, csv, or
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
-    years = ['2013','2015']
     for year in years:
         ####Host#########
         print("Getting Host")
@@ -92,6 +89,9 @@ def pre_ceremony():
         for award in OFFICIAL_AWARDS:
             result[award] = fres[award]["Presenters"]
         ##################
+        ####Nominees######
+        print('Getting Nominees')
+        nominees = gs.get_nominees(year)
         ####Winner#########
         print("Getting Winners")
         winners = gw.get_winner(year)
@@ -103,6 +103,7 @@ def pre_ceremony():
             print()
             print('Award:',j)
             print('Presenters:',result[j])
+            print('Nominees:',nominees[j])
             print('Winner:',winners[j])
         print()
         print('Best Dressed: ',h)
@@ -113,7 +114,7 @@ def pre_ceremony():
             print('Common sentiments to host',h ,':' ,sent[h]['words'])
         
         with open('gg%sresults.json' % year, 'w', encoding='utf8') as outfile:
-            json.dump({'host':hosts,'awards':awards,'presenters':result,'winner':winners,'Best Dressed:':h,
+            json.dump({'host':hosts,'awards':awards,'presenters':result,'nominees':nominees,'winner':winners,'Best Dressed:':h,
                         'Worst Dressed:':l,'sentiment':sent}, outfile)
     print("Pre-ceremony processing complete.")
     return
@@ -125,8 +126,12 @@ def main():
     run when grading. Do NOT change the name of this function or
     what it returns.'''
     # Your code here
-    pre_ceremony()
+    if len(sys.argv)<2:
+        print('Please give a year')
+    else:
+        pre_ceremony(sys.argv[1:])
     return
 
 if __name__ == '__main__':
     main()
+    
